d051c64edca11612a61180627d3d3c906bf67718
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-03-26 17:21:00 +0200

    changed hashpassword function in bcrypt to promise

--- src/utils/bcrypt/index.js
+++ src/utils/bcrypt/index.js
@@ -1,13 +1,7 @@
 const bcrypt = require("bcrypt");
 
 // eslint-disable-next-line no-undef
-const hashPassword = bcrypt.hash(password, 10, (err, hash) => {
-  if (err) {
-    console.log(err, " from hashpassword");
-  } else {
-    return hash;
-  }
-});
+const hashPassword = bcrypt.hash(password, 10);
 
 const comparePassword = (password, hash) => bcrypt.compare(password, hash);
 module.exports = {
