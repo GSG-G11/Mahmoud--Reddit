01e8347aa1db02c0f701810b96f35b2a75ab3374
Mahmoud Alhato <mahmoudalhato2@gmail.com>
2022-03-28 15:31:14 +0300

    handle all add post functions in frontend and connect it with backend Relates #19

--- public/js/script.js
+++ public/js/script.js
@@ -5,10 +5,15 @@ const addCommentSection = document.querySelector(".add-comment-section");
 const addCommentButton = document.querySelector(".add-comment-button");
 const comment = document.querySelector(".comment-form-input");
 const errorMessage = document.querySelector("#errors-message");
+const errorPostMessage = document.querySelector("#errors-message-post");
 const closePost = document.querySelector(".cancel");
 const openPost = document.querySelector(".open-button");
+const addPostBtn = document.querySelector(".btn-add-post");
+const titlePost = document.querySelector(".title-input-form");
+const describtionPost = document.querySelector(".describtion-input-form");
 
 let inputError = [];
+
 showComments.addEventListener("click", (e) => {
   e.preventDefault();
   if (allComments.style.display === "block") {
@@ -17,6 +22,7 @@ showComments.addEventListener("click", (e) => {
     allComments.style.display = "block";
   }
 });
+
 addComment.addEventListener("click", (e) => {
   e.preventDefault();
   if (addCommentSection.style.display === "block") {
@@ -49,7 +55,53 @@ openPost.addEventListener("click", (e) => {
   e.preventDefault();
   document.getElementById("myForm").style.display = "block";
 });
+
 closePost.addEventListener("click", (e) => {
   e.preventDefault();
   document.getElementById("myForm").style.display = "none";
+});
+
+addPostBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (titlePost.value.trim() === "") {
+    inputError.push("Title is required");
+  }
+  if (describtionPost.value.trim() === "") {
+    inputError.push("Describtion is required");
+  }
+  while (errorPostMessage.firstChild) {
+    errorPostMessage.removeChild(errorPostMessage.lastChild);
+  }
+  if (inputError.length !== 0) {
+    inputError.forEach((e) => {
+      const msg = document.createElement("h3");
+      msg.textContent = e;
+      errorPostMessage.appendChild(msg);
+    });
+    inputError = [];
+    return;
+  }
+  const values = {
+    title: titlePost.value,
+    description: describtionPost.value,
+  };
+  fetch("/post", {
+    method: "post",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(values),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.message === "Post successfully added") {
+        console.log("posted successfully");
+      } else {
+        const msg = document.createElement("h3");
+        msg.textContent = res.message;
+        errorPostMessage.appendChild(msg);
+      }
+    })
+    .then(() => {
+      titlePost.value = "";
+      describtionPost.value = "";
+    });
 });
